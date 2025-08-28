a504832facd114b000b9864a0f57a3daf83eaf3a
Milles Jethro Del Rosario <milles.jethro16@gmail.com>
2025-08-28 23:20:29 +0800

    new game

---
+++ scripts/player/PlayerScript.gd
@@ -0,0 +1,41 @@
+extends CharacterBody2D
+
+@onready var PlayerSprite: AnimatedSprite2D = $LancerSprite
+
+@export var GravityForce: float = 600.0 # downward pull
+@export var MoveSpeed: float = 200.0   # movement speed
+
+func _ready() -> void:
+	PlayerSprite.play("idle")
+
+func _physics_process(delta: float) -> void:
+	PlayerMovement(delta)
+
+# --------------------------------
+# Player Movement Function
+# --------------------------------
+func PlayerMovement(DeltaForce: float):
+	if not is_on_floor():
+		velocity.y += GravityForce * DeltaForce
+	else:
+		velocity.y = 0  # stay grounded when on floor
+	
+	# Left/Right input
+	var Direction = Input.get_axis("ui_left", "ui_right")
+	velocity.x = Direction * MoveSpeed
+		# Flip + Animation
+	PlayerMovementSprite(Direction)
+	move_and_slide()
+
+# --------------------------------
+# Player Sprite Functions
+# --------------------------------
+func PlayerMovementSprite(Direction: int):
+	if Direction > 0:
+		PlayerSprite.flip_h = false
+		PlayerSprite.play("walking")
+	elif Direction < 0:
+		PlayerSprite.flip_h = true
+		PlayerSprite.play("walking")
+	else:
+		PlayerSprite.play("idle")
